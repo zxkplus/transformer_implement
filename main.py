551ff1d083c7e37cab82382b844f3aa999b33f58
zengxinke <zengxinke@industai.com>
2026-07-27 17:26:03 +0800

    feat: add EncoderBlock and TransformerEncoder

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def scaled_dot_product_attention(q, k, v, mask=None):
     return o , attn
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, num_heads,):
+    def __init__(self, d_model, num_heads):
         super().__init__()
         self.d_model  = d_model
         self.num_heads = num_heads
@@ -63,5 +63,20 @@ class PositionwiseFeedForward(nn.Module):
     def forward(self,x):
         return self.line2(torch.relu(self.line1(x)))
 
-        
-    
+class EncoderBlock(nn.Module):
+    def __init__(self,d_model,num_heads):
+        super().__init__()
+        self.multihead = MultiHeadAttention(d_model,num_heads)
+        self.ffn = PositionwiseFeedForward(d_model)
+        self.layernorm = torch.nn.LayerNorm(d_model)
+    def forward(self,x):
+        x1 = self.layernorm(x + self.multihead(x,x,x)[0])
+        x2 = self.layernorm(x1 + self.ffn(x1))
+        return x2
+
+class TransformerEncoder(nn.Module):
+    def __init__(self,d_model,num_heads,num_layers):
+        super().__init__()
+        self.encodesequence = nn.Sequential(*[EncoderBlock(d_model,num_heads) for i in range(num_layers)])
+    def forward(self,x):
+        return self.encodesequence(x)
